24cc3547af32607ef7328f5072c93ed0bca5168d
hex-aragon <0xaragon90@gmail.com>
2024-01-24 23:07:51 +0900

    ADD: rock, scissors, paper v1

---
+++ rock_paper_scissors/run.sh
@@ -0,0 +1,15 @@
+#1번째 유저는 주먹, 2번째 유저는 가위를 낸 상황 
+leo run main "{ first: 0field,  second: 2field }"
+
+#결과는 다음과 같이 출력된다.
+#Leo ✅ Compiled 'main.leo' into Aleo instructions
+
+#⛓  Constraints
+
+# •  'rock_paper_scissors.aleo/main' - 54 constraints (called 1 time)
+
+#➡️  Output
+
+# • 0field
+
+#       Leo ✅ Finished 'rock_paper_scissors.aleo/main' (in "/home/robert/work/aleo/rock_paper_scissors/build")
